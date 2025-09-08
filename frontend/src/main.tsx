9f57b8bf004d096d7bf0fa90b32cb18fccd5bd92
ppower-dev <smothchoco@gmail.com>
2025-09-08 09:37:53 +0900

    feat : Pretendard 폰트 설치 및 전역 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import 'pretendard/dist/web/static/pretendard.css'
+import './assets/fonts.css'
 import './assets/colors.css'
 
 createRoot(document.getElementById('root')!).render(
