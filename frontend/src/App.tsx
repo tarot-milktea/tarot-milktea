88fb5423bf49fd66c160f9984071c9267a6c1167
ppower-dev <smothchoco@gmail.com>
2025-09-18 15:16:38 +0900

    feat : 로고 아이콘

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import OnboardingFlow from './components/OnboardingFlow';
 import ResultPage from './pages/ResultPage';
 import ErrorPage from './pages/ErrorPage';
+import TempOnboardingPage from './pages/TempOnboardingPage';
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
       <Routes>
         {/* 온보딩 플로우는 useState 방식 */}
         <Route path="/" element={<OnboardingFlow />} />
-        
+
+        {/* 임시 온보딩 페이지 - 디자인 테스트용 */}
+        <Route path="/temp-onboarding" element={<TempOnboardingPage />} />
+
         {/* 결과 페이지만 라우팅 */}
         <Route path="/result/:resultId" element={<ResultPage />} />
 
