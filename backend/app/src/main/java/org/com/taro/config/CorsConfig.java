92d38b4366f87da0a8db5dde1a7e8c42fc16e688
nicecorntea <nicecorntea@naver.com>
2025-09-26 12:46:06 +0900

    fix : cors error on SSE

--- backend/app/src/main/java/org/com/taro/config/CorsConfig.java
+++ backend/app/src/main/java/org/com/taro/config/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("*");
     }
 }
