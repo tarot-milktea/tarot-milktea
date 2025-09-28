d9f012ccb552f1c680467102c10b97c877adac11
se0hyun <be8shyn@gmail.com>
2025-09-28 09:47:39 +0900

    add : origin 추가

--- backend/app/src/main/java/org/com/taro/config/CorsConfig.java
+++ backend/app/src/main/java/org/com/taro/config/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://127.0.0.1:5173",
-                        "https://j13a601.p.ssafy.io"
+                        "https://j13a601.p.ssafy.io",
+                        "https://tarotmilk.com",
+                        "https://www.tarotmilk.com",
+                        "http://tarotmilk.com",
+                        "http://www.tarotmilk.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
